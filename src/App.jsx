93cb0285cfd822a585a5881a6781272074582236
Ruas <ruasc@merck.com>
2022-09-28 08:21:59 -0300

    adding form to create new questions

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { supabase } from './client'
 
 const Question = ({ question }) => {
 	console.log(question)
-	const { statement, answer } = question
+	const { statement, answer, created_at } = question
 	return <div className='question'>
+		<div>{new Date(created_at).toLocaleString()}</div>
 		<div className='statement'>
 			<span>Pergunta:</span>
 			<p>{statement}</p>
@@ -16,28 +17,58 @@ const Question = ({ question }) => {
 	</div>
 }
 
-const Questions = () => {
-	const [questions, setQuestions] = useState([])
-	useEffect(()=>{
-		async function getQuestions(){
-			let { data: questions, error } = await supabase
-  				.from('questions')
-  				.select('*')
-			setQuestions(questions)
-		}
-		getQuestions()
-	}, [])
+const Questions = ({ questions }) => {
 	return <div className='questions'>
 		{questions.map(question => <Question key={question.id} question={question}/>)}
 	</div>
-} 
+}
+
+const InsertQuestion = ({ onChange }) => {
+	const [question, setQuestion] = useState({ statement: '', answer: '' })
+	const { statement, answer } = question
+	const handleClick = e => {
+		async function createQuestion(){
+			await supabase.from('questions').insert([{ statement, answer }]).single()
+			onChange()
+			setQuestion({ statement: '', answer: '' })
+		}
+		createQuestion()
+	}
+	return <div className='question-form'>
+		<div className='field'>
+			<label for="statement">Statement</label>
+			<textarea id="statement" 
+				value={statement}
+				onChange={({ target })=>setQuestion({ ...question, statement: target.value })}></textarea>
+		</div>
+		<div className='field'>
+			<label for="answer">Answer</label>
+			<textarea id="answer" 
+				value={answer} 
+				onChange={({ target })=>setQuestion({ ...question, answer: target.value })}></textarea>
+		</div>
+		<button onClick={handleClick}>Insert new question</button>
+	</div>
+}
 
 function App() {
-	
+	const [questions, setQuestions] = useState([])
+	const [update, setUpdate] = useState(false)
+	async function getQuestions(){
+		let { data: questions, error } = await supabase
+			  .from('questions')
+			  .select('*')
+			.order('id', { ascending: false })
+		setQuestions(questions)
+	}
+	useEffect(()=>{
+		getQuestions()
+	}, [])
 	return (
 		<div className="App">
 			<h1>Axonifier 3.0</h1>
-			<Questions />
+			<InsertQuestion onChange={e => getQuestions()} />
+			<Questions questions={questions} />
 		</div>
 	)
 }
