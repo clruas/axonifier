2b0533c037bbe3f2643bb079d404b5e43de5af36
Ruas <ruasc@merck.com>
2022-09-29 00:25:41 -0300

    Adding search questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import { useState, useEffect } from 'react'
 import { supabase } from './client'
+import './app.sass'
 
 const Question = ({ question }) => {
-	console.log(question)
-	const { statement, answer, created_at } = question
+	const { id, statement, answer, created_at } = question
 	return <div className='question'>
+		<div>{id}</div>
 		<div>{new Date(created_at).toLocaleString()}</div>
 		<div className='statement'>
 			<span>Pergunta:</span>
-			<p>{statement}</p>
+			<p style={{ whiteSpace: 'pre-line' }}>{statement}</p>
 		</div>
 		<div className='answer'>
 			<span>Resposta:</span>
-			<p>{answer}</p>
+			<p style={{ whiteSpace: 'pre-line' }}>{answer}</p>
 		</div>
+		<hr/>
 	</div>
 }
 
 const Questions = ({ questions }) => {
+	const [filterText, setFilterText] = useState('')
+	let filteredQuestions = questions.filter(question => question.statement.toLowerCase().includes(filterText.toLowerCase()))
 	return <div className='questions'>
-		{questions.map(question => <Question key={question.id} question={question}/>)}
+		<div className='filter'>
+			<input type="search" placeholder='Pesquisar' value={filterText} onChange={e => setFilterText(e.target.value)}/>
+		</div>
+		<div className='summary'>{filteredQuestions.length} perguntas listadas.</div>
+		<div className='items'>
+			{filteredQuestions.map(question => <Question key={question.id} question={question}/>)}
+		</div>
 	</div>
 }
 
